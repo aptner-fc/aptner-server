f6e072448c6406efe0df83509a419c9d0351a168
John <jinyngg@gmail.com>
2024-04-27 16:32:06 +0900

    [#1] FEAT : 공통 응답 및 코드 생성

---
+++ src/main/java/com/fc8/aptner/common/code/ErrorCode.java
@@ -0,0 +1,23 @@
+package com.fc8.aptner.common.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    CONSTRAINT_VIOLATION(HttpStatus.CONFLICT, "제약 조건 위반"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류가 발생하였습니다."),
+
+    /**
+     * Common
+     */
+    COMMON_INVALID_PARAM(HttpStatus.BAD_REQUEST, "요청한 값이 올바르지 않습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
