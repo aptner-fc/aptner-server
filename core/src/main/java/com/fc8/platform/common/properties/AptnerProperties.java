07b7227bf5f3f3012cf17163c99f60558fbd5305
John <jinyngg@gmail.com>
2024-05-20 16:15:53 +0900

    feat : 프로퍼티 공통 관리를 위한 추상 클래스 추가

---
+++ core/src/main/java/com/fc8/platform/common/properties/AptnerProperties.java
@@ -0,0 +1,9 @@
+package com.fc8.platform.common.properties;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public abstract class AptnerProperties {
+
+}
