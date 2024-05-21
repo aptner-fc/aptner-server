e4d8e0c2ee65ec956a74a22b7cf9cdd4a37dee9c
유현 <yh7119@gmail.com>
2024-05-21 14:10:56 +0900

    Fix: 무중단 배포 시 4xx 에러 방지를 위한 DefaultController 생성

---
+++ client/src/main/java/com/fc8/application/DefaultController.java
@@ -0,0 +1,17 @@
+package com.fc8.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 무중단 배포 시 4xx 에러 방지를 위함
+ */
+
+@RestController
+public class DefaultController {
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>aptner<h1>";
+    }
+}
