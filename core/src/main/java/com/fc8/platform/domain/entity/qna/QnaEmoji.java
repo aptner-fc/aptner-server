879cb22f4af099957a2ad5d18ef83f14746be3ad
유현 <yh7119@gmail.com>
2024-05-25 15:13:39 +0900

    Feat: 민원 게시판 이모지 엔티티 생성

---
+++ core/src/main/java/com/fc8/platform/domain/entity/qna/QnaEmoji.java
@@ -0,0 +1,35 @@
+package com.fc8.platform.domain.entity.qna;
+
+import com.fc8.platform.domain.entity.member.Member;
+import com.fc8.platform.domain.enums.EmojiType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table(
+    name = "qna_emoji"
+)
+public class QnaEmoji {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "bigint unsigned comment '이모지 고유 번호'")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "qna_id", columnDefinition = "bigint unsigned comment '민원 게시판 ID'")
+    private Qna qna;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", columnDefinition = "bigint unsigned comment '회원 ID'")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "emoji", nullable = false, columnDefinition = "varchar(20) comment '이모지'")
+    private EmojiType emoji;
+
+}
