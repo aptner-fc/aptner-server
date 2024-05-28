78a97caf9e86677b7ccf4288587597490eda26bc
유현 <yh7119@gmail.com>
2024-05-28 20:29:27 +0900

    Fix: @Getter 추가

--- core/src/main/java/com/fc8/platform/dto/response/LoadQnaListResponse.java
+++ core/src/main/java/com/fc8/platform/dto/response/LoadQnaListResponse.java
@@ -2,9 +2,11 @@ package com.fc8.platform.dto.response;
 
 import com.fc8.platform.dto.record.PinnedQna;
 import com.fc8.platform.dto.record.QnaInfo;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 public class LoadQnaListResponse {
 
     List<QnaInfo> qnas;
