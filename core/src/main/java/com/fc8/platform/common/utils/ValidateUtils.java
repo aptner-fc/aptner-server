36eca794eb550c299d2abb4ecb9019a8df537cf0
John <jinyngg@gmail.com>
2024-05-29 00:24:51 +0900

    [#20] feat : 카테고리 유효성 검사 추가
    1. Jwt Exception 추가

--- core/src/main/java/com/fc8/platform/common/utils/ValidateUtils.java
+++ core/src/main/java/com/fc8/platform/common/utils/ValidateUtils.java
@@ -2,9 +2,13 @@ package com.fc8.platform.common.utils;
 
 import com.fc8.platform.common.exception.InvalidParamException;
 import com.fc8.platform.common.exception.code.ErrorCode;
+import com.fc8.platform.domain.entity.category.Category;
+import com.fc8.platform.domain.enums.CategoryType;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ValidateUtils {
@@ -16,4 +20,19 @@ public class ValidateUtils {
             throw new InvalidParamException(ErrorCode.FAIL_LOGIN);
         }
     }
+
+    public static void validateChildCategoryType(CategoryType type, Category category) {
+        isChildCategory(category);
+
+        Optional.ofNullable(type)
+                .filter(t -> t.equals(category.getType()))
+                .orElseThrow(() -> new InvalidParamException(ErrorCode.NOT_FOUND_CATEGORY));
+    }
+
+    private static void isChildCategory(Category category) {
+        if (category.getParent() == null) {
+            throw new InvalidParamException(ErrorCode.NOT_FOUND_CATEGORY);
+        }
+    }
+
 }
