2f3ec2bb2bdf40bdfcca52adc5d7de741785fec1
유현 <yh7119@gmail.com>
2024-05-31 01:09:28 +0900

    feat: 민원 처리상태 추가

--- core/src/main/java/com/fc8/platform/domain/entity/qna/Qna.java
+++ core/src/main/java/com/fc8/platform/domain/entity/qna/Qna.java
@@ -6,6 +6,7 @@ import com.fc8.platform.domain.BaseApartEntity;
 import com.fc8.platform.domain.entity.apartment.Apart;
 import com.fc8.platform.domain.entity.category.Category;
 import com.fc8.platform.domain.entity.member.Member;
+import com.fc8.platform.domain.enums.ProcessingStatus;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -37,7 +38,9 @@ public class Qna extends BaseApartEntity {
     @Column(name = "content", nullable = false, columnDefinition = "mediumtext comment '내용'")
     private String content;
 
-    // 게시글 상태 추가 TODO
+    @Enumerated(EnumType.STRING)
+    @Column(name = "processing_status", nullable = false, columnDefinition = "varchar(20) comment '처리 상태'")
+    private ProcessingStatus processingStatus;
 
     @ManyToOne
     @JoinColumn(name = "member_id", columnDefinition = "bigint unsigned comment '회원 ID'")
@@ -49,6 +52,7 @@ public class Qna extends BaseApartEntity {
     @Column(name = "is_private", nullable = false, columnDefinition = "tinyint(1) comment '비밀글 여부'")
     private boolean isPrivate;
 
+
     public static Qna create(
         Category category, Member member, Apart apart, String title, String content, boolean isPrivate
     ) {
@@ -58,6 +62,7 @@ public class Qna extends BaseApartEntity {
             .title(title)
             .content(content)
             .isPrivate(isPrivate)
+            .processingStatus(ProcessingStatus.접수)
             .build();
         qna.updateApart(apart);
 
---
+++ core/src/main/java/com/fc8/platform/domain/enums/ProcessingStatus.java
@@ -0,0 +1,5 @@
+package com.fc8.platform.domain.enums;
+
+public enum ProcessingStatus {
+    접수, 처리중, 처리완료
+}
