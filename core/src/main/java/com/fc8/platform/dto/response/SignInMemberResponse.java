252a45e0b67dbb2e46c410c13afcc26c0e1c9ed8
John <jinyngg@gmail.com>
2024-06-02 21:08:43 +0900

    [#40] style : 로그인 정보 변경

--- core/src/main/java/com/fc8/platform/dto/response/SignInMemberResponse.java
+++ core/src/main/java/com/fc8/platform/dto/response/SignInMemberResponse.java
@@ -1,6 +1,5 @@
 package com.fc8.platform.dto.response;
 
-import com.fc8.platform.domain.enums.MemberStatus;
 import com.fc8.platform.dto.record.MemberInfo;
 import com.fc8.platform.dto.record.TokenInfo;
 import lombok.Getter;
@@ -8,12 +7,12 @@ import lombok.Getter;
 @Getter
 public class SignInMemberResponse {
 
-    private final SignInMember member;
+    private final SignInMember loginMember;
     private final TokenInfo token;
 
 
-    public SignInMemberResponse(SignInMember member, TokenInfo token) {
-        this.member = member;
+    public SignInMemberResponse(SignInMember loginMember, TokenInfo token) {
+        this.loginMember = loginMember;
         this.token = token;
     }
 
@@ -23,13 +22,11 @@ public class SignInMemberResponse {
         private final Long id;
         private final String email;
         private final String nickname;
-        private final MemberStatus status;
 
         public SignInMember(MemberInfo memberInfo) {
             this.id = memberInfo.id();
             this.email = memberInfo.email();
             this.nickname = memberInfo.nickname();
-            this.status = memberInfo.status();
         }
     }
 }
