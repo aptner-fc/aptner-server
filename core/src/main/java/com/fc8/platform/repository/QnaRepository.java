6952506da38ff906d50e57b7291c44f47bae0b92
유현 <yh7119@gmail.com>
2024-06-03 19:40:54 +0900

    feat: 민원 목록 조회 시 카테고리별 글 조회 기능 추가

--- core/src/main/java/com/fc8/platform/repository/QnaRepository.java
+++ core/src/main/java/com/fc8/platform/repository/QnaRepository.java
@@ -2,6 +2,7 @@ package com.fc8.platform.repository;
 
 import com.fc8.platform.domain.entity.member.Member;
 import com.fc8.platform.domain.entity.qna.Qna;
+import com.fc8.platform.domain.enums.SearchType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -9,7 +10,7 @@ public interface QnaRepository {
 
     Qna store(Qna qna);
 
-    Page<Qna> getQnaListByApartCode(Long memberId, String apartCode, Pageable pageable, String search);
+    Page<Qna> getQnaListByApartCode(Long memberId, String apartCode, Pageable pageable, String search, SearchType type, String categoryCode);
 
     Qna getQnaWithCategoryByIdAndApartCode(Long qnaId, String apartCode);
 
