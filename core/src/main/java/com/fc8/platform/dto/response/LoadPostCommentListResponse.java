151140bd3c9900f42068f8b0a0db945ed6a6c6f3
유현 <yh7119@gmail.com>
2024-06-03 22:14:01 +0900

    fix: 댓글 목록 조회 에러 해결

--- core/src/main/java/com/fc8/platform/dto/response/LoadPostCommentListResponse.java
+++ core/src/main/java/com/fc8/platform/dto/response/LoadPostCommentListResponse.java
@@ -1,9 +1,11 @@
 package com.fc8.platform.dto.response;
 
 import com.fc8.platform.dto.record.PostCommentInfo;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 public class LoadPostCommentListResponse {
 
     List<PostCommentInfo> comments;
