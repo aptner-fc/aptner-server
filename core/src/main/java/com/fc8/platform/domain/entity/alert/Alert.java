7e0e9433d26486f10e1e0447d377d5befe35a15a
유현 <yh7119@gmail.com>
2024-06-05 16:06:26 +0900

    feat: 알림 엔티티 추가 및 삭제, 등록 기능 추가

---
+++ core/src/main/java/com/fc8/platform/domain/entity/alert/Alert.java
@@ -0,0 +1,35 @@
+package com.fc8.platform.domain.entity.alert;
+
+import com.fc8.platform.domain.BaseTimeEntity;
+import com.fc8.platform.domain.entity.member.Member;
+import com.fc8.platform.domain.enums.AlertStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table(
+    name = "alert"
+)
+public class Alert extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED comment '알림 고유 번호'")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", columnDefinition = "bigint unsigned comment '회원 ID'")
+    private Member member;
+
+    @Column(name = "content", nullable = false, columnDefinition = "mediumtext comment '내용'")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, columnDefinition = "varchar(20) comment '알림 상태'")
+    private AlertStatus status;
+
+}
