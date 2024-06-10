27527e21ca254f05c37c1a95bed638a5d9d44ae6
유현 <yh7119@gmail.com>
2024-06-10 22:57:12 +0900

    refactor: 게시글 개수 응답구조 변경

--- core/src/main/java/com/fc8/platform/dto/response/LoadUnifiedListResponse.java
+++ core/src/main/java/com/fc8/platform/dto/response/LoadUnifiedListResponse.java
@@ -12,39 +12,37 @@ import java.util.List;
 public class LoadUnifiedListResponse {
 
     private final Long noticeCount;
-    private final Long disclosureCount;
-    private final Long postCount;
-    private final Long qnaCount;
     private final List<SearchNoticeInfo> pinnedNoticeList;
     private final List<SearchNoticeInfo> noticeList;
+
+    private final Long disclosureCount;
     private final List<SearchDisclosureInfo> pinnedDisclosureList;
     private final List<SearchDisclosureInfo> disclosureList;
+
+    private final Long postCount;
     private final List<SearchPostInfo> pinnedPostList;
     private final List<SearchPostInfo> postList;
+
+    private final Long qnaCount;
     private final List<SearchQnaInfo> pinnedQnaList;
     private final List<SearchQnaInfo> qnaList;
 
     public LoadUnifiedListResponse(
-        Long noticeCount, Long disclosureCount, Long postCount, Long qnaCount,
-        List<SearchNoticeInfo> pinnedNoticeList,
-        List<SearchNoticeInfo> noticeList,
-        List<SearchDisclosureInfo> pinnedDisclosureList,
-        List<SearchDisclosureInfo> disclosureList,
-        List<SearchPostInfo> pinnedPostList,
-        List<SearchPostInfo> postList,
-        List<SearchQnaInfo> pinnedQnaList,
-        List<SearchQnaInfo> qnaList
+        Long noticeCount, List<SearchNoticeInfo> pinnedNoticeList, List<SearchNoticeInfo> noticeList,
+        Long disclosureCount, List<SearchDisclosureInfo> pinnedDisclosureList, List<SearchDisclosureInfo> disclosureList,
+        Long postCount, List<SearchPostInfo> pinnedPostList, List<SearchPostInfo> postList,
+        Long qnaCount, List<SearchQnaInfo> pinnedQnaList, List<SearchQnaInfo> qnaList
     ) {
         this.noticeCount = noticeCount;
-        this.disclosureCount = disclosureCount;
-        this.postCount = postCount;
-        this.qnaCount = qnaCount;
         this.pinnedNoticeList = pinnedNoticeList;
         this.noticeList = noticeList;
+        this.disclosureCount = disclosureCount;
         this.pinnedDisclosureList = pinnedDisclosureList;
         this.disclosureList = disclosureList;
+        this.postCount = postCount;
         this.pinnedPostList = pinnedPostList;
         this.postList = postList;
+        this.qnaCount = qnaCount;
         this.pinnedQnaList = pinnedQnaList;
         this.qnaList = qnaList;
     }
